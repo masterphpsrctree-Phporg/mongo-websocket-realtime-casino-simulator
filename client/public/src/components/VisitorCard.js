d564e6a0572c26d8d8efead201fa1a9a40514e64
masterphpsrctree-Phporg <masterphpsrctree@gmail.com>
2026-03-18 06:27:01 +0600

    Update VisitorCard.js

--- client/public/src/components/VisitorCard.js
+++ client/public/src/components/VisitorCard.js
@@ -1,1 +1,13 @@
+import React from "react";
 
+function VisitorCard({ visitor, startFlow }) {
+  return (
+    <div style={{ padding: "15px", border: "2px solid #2ecc71", borderRadius: "8px", width: "150px", textAlign: "center" }}>
+      <h3>{visitor.name}</h3>
+      <p>Step: {visitor.step || "Idle"}</p>
+      <button onClick={() => startFlow(visitor.id)}>Start Flow</button>
+    </div>
+  );
+}
+
+export default VisitorCard;
