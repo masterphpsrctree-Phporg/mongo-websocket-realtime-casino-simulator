a03e1da5f88429deb82bff9a569dec31ff66a6c4
masterphpsrctree-Phporg <masterphpsrctree@gmail.com>
2026-03-18 06:22:03 +0600

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const mongoURI = "mongodb+srv://masterphpsrctree-phporg:NsYivSZSoWZdZOQZuEkOZfTKGLlAWEuv@cluster0.mongodb.net/casinoDB?retryWrites=true&w=majority";
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+module.exports = mongoose;
